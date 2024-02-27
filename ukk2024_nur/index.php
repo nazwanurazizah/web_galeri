a2b9d6b5e2b80808f93f1e1ef86bad47bfaf65b2
nazwa nurazizah <156876806+nazwanurazizah@users.noreply.github.com>
2024-02-27 09:46:54 +0700

    Add files via upload

---
+++ ukk2024_nur/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Galeri Foto</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+   <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-primary m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+<!-- Isi Halaman -->
+<div class="container" mt-3>
+<div class="row">
+<div class="col-md-3">
+<div class="card">
+<img style="height: 12rem;" src="EXO.jpg" class="card-img-top" title="">
+<div class="card-footer text-center">
+    <a href=""><i class="fa-regular fa-heart m-1"></i></a> 10 Suka
+    <a href=""><i class="fa-regular fa-comment"></i></a> 15 Komentar
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<footer class="d-flex justyfy-content-center border-top mt-3 bg-light fixed-bottom">
+    <p>&copy; UKK RPL 2024 | Nazwa Nurazizah</p>
+</footer>
+
+
+<script type="text/javascript" src="assets/js/bootsrap.min.js"></script>
+</body>
+</html>
